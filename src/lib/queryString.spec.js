9847a112293f151b7a80a9eddddaa49a5f5fd2c4
Renan Garcia <renan.garcia@brainweb.com.br>
2022-03-16 20:53:23 -0300

    Accepting arrays but not objects as values

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -8,4 +8,28 @@ describe('Object to query string', () => {
     };
     expect(queryString(obj)).toBe('name=Renan&isAnOldMan=false');
   });
+
+  it('should create a valid qyert string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Renan',
+      abilities: ['javascript', 'typescript', 'react'],
+    };
+    expect(queryString(obj)).toBe(
+      'name=Renan&abilities=javascript,typescript,react'
+    );
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Renan',
+      abilities: {
+        first: 'javascript',
+        second: 'typescript',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
--- src/lib/queryString.ts
+++ src/lib/queryString.ts
@@ -2,8 +2,13 @@ type QueryStringObj = {
   [key: string]: boolean | string | number | undefined | null;
 };
 
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error(`Invalid value type: ${typeof value}`);
+  }
+  return `${key}=${value}`;
+};
+
 export default function queryString(obj: QueryStringObj) {
-  return Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
+  return Object.entries(obj).map(keyValueToString).join('&');
 }
